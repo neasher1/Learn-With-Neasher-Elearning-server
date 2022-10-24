7a0f1e5dc7a68647dae5d6b5572a51ee41480364
developerorin2002 <developerorin2002@gmail.com>
2022-10-25 01:36:27 +0600

    server side done

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 
-const allCourses = require('./courseData/courseData.json')
+// get all category 
+const allcategory = require('./courseData/courseCategory.json');
+app.get('/category',(req,res)=>{
+    res.send(allcategory);
+})
 
+
+// get all course
+const allCourses = require('./courseData/courseData.json')
 app.get('/',(req ,res)=>{
     res.send(allCourses);
 })
 
+// get course by course_category
 
+app.get('/courses/:category',(req,res)=>{
+    const categoryName = req.params.category;
+    console.log(categoryName);
+    const fillterdCourse = allCourses.filter(course=>course.course_category === categoryName);
+    res.send(fillterdCourse);
+});
+
+// get course by unique id 
+
+app.get('/course/:id',(req,res)=>{
+    const getedId = req.params.id;
+    console.log(getedId)
+    const findCourses = allCourses.find(course => course.id == getedId);
+    res.send(findCourses);
+})
 
 
 app.listen(port,()=>{
