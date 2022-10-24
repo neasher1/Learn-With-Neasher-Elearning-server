3b38e7a49daad2c1b74ceba06cdb2b492be5b7d8
developerorin2002 <developerorin2002@gmail.com>
2022-10-25 00:46:17 +0600

    created some API

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+
+app.get('/',(req ,res)=>{
+    res.send('server running');
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
