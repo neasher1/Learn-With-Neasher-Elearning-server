dbe7da88bb67d0204ff8f4e189e74ba6bbc1df82
developerorin2002 <developerorin2002@gmail.com>
2022-10-25 00:48:29 +0600

    api image link change

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 
+const allCourses = require('./courseData/courseData.json')
+
 app.get('/',(req ,res)=>{
-    res.send('server running');
+    res.send(allCourses);
 })
 
 
